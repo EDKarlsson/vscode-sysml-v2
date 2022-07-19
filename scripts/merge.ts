165eee4a0676e69a803ea404c0ea6aed880d1b47
Erik Karlsson <erikdkarlsson8@gmail.com>
2022-07-19 12:58:50 -0700

    Added functionality for merging the generated langium tmLanguage file and my own. Allows for customization of the syntax highlighting.

--- scripts/merge.ts
+++ scripts/merge.ts
@@ -1,37 +1,12 @@
-import { mergeFile } from "json-merger";
-var result = mergeFile("a.json");
-console.log(result);
+import { writeFileSync } from 'fs'
+import { mergeObject } from 'json-merger'
 
-import jsonData from '../syntaxes/sysml.extend.tmLanguage.json';
-console.log(jsonData);
+import template from '../syntaxes/template.tmLanguage.json'
 
-// var obj_a_1 = 
-// {
-//   "$merge": {
-//     "source": {
-//       "$import": "b.json"
-//     },
-//     "with": {
-//       "prop1": {
-//         "$replace": {
-//           "prop1a": "this will replace b.json's property prop1"
-//         }
-//       },
-//       "prop2": {
-//         "prop2a": "this will merge with b.json's property prop2"
-//       }
-//     }
-//   }
-// };
+const merged = JSON.stringify(mergeObject(template), null, 2)
 
-// var obj_b_1 = 
-// {
-//   "prop1": {
-//     "prop1b": "will be replaced"
-//   },
-//   "prop2": {
-//     "prop2b": "will be merged"
-//   }
-// };
+const tmLanguageFile = 'syntaxes/sysml.tmLanguage.json'
 
-// var result = jsonMerger.mergeObjects([obj_a_1, obj_b_1]);
+writeFileSync(tmLanguageFile, merged, {
+    flag: 'w',
+})
